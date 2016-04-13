c49ad6ab44f7b1c6f67048cfee3fc833bf5e5b0a
Pawel M <obramanix@gmail.com>
2016-04-14 00:12:35 +0200

    two pics

--- stereo_matching/Source.cpp
+++ stereo_matching/Source.cpp
@@ -53,6 +53,13 @@ int main()
   filter[i] /= 13.0f;
  }
 
+
+ Image imgL;
+ lodepng::decode(imgL.pixel, imgL.width, imgL.height, "img/view1.png");
+ Image imgR;
+ lodepng::decode(imgR.pixel, imgR.width, imgR.height, "img/view2.png");
+
+
  // Create a program from source
  cl_program program = CreateProgram(LoadKernel("kernels/image.cl"),  context);
 
@@ -60,34 +67,36 @@ int main()
 
  cl_kernel kernel = clCreateKernel(program, "Filter", &error);
 
- Image img;
- lodepng::decode(img.pixel,img.width,img.height, "img/view1.png");
 
 
  static const cl_image_format format = { CL_RGBA, CL_UNORM_INT8 };
- cl_mem inputImage = clCreateImage2D(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, &format,  img.width, img.height, 0,  // This is a bug in the spec
-  const_cast<unsigned char*> (img.pixel.data()),  &error);
+ cl_mem inputImage = clCreateImage2D(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, &format,  imgL.width, imgL.height, 0,  // This is a bug in the spec
+  const_cast<unsigned char*> (imgL.pixel.data()),  &error);
+ cl_mem inputImage2 = clCreateImage2D(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, &format, imgR.width, imgR.height, 0,  // This is a bug in the spec
+  const_cast<unsigned char*> (imgR.pixel.data()), &error);
 
- cl_mem outputImage = clCreateImage2D(context, CL_MEM_WRITE_ONLY, &format,  img.width, img.height, 0,  nullptr, &error);
+ cl_mem outputImage = clCreateImage2D(context, CL_MEM_WRITE_ONLY, &format,  imgL.width, imgL.height, 0,  nullptr, &error);
 
  cl_mem filterWeightsBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,  sizeof(float) * 16, filter, &error);
 
  clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputImage);
- clSetKernelArg(kernel, 1, sizeof(cl_mem), &filterWeightsBuffer);
- clSetKernelArg(kernel, 2, sizeof(cl_mem), &outputImage);
+ clSetKernelArg(kernel, 1, sizeof(cl_mem), &inputImage2);
+ clSetKernelArg(kernel, 2, sizeof(cl_mem), &filterWeightsBuffer);
+ clSetKernelArg(kernel, 3, sizeof(cl_mem), &outputImage);
 
  cl_command_queue queue = clCreateCommandQueue(context, deviceIds[0],  0, &error);
  
  std::size_t offset[3] = { 0 };
- std::size_t size[3] = { img.width, img.height, 1 };
+ std::size_t size[3] = { imgL.width, imgL.height, 1 };
  clEnqueueNDRangeKernel(queue, kernel, 2, offset, size, nullptr, 0, nullptr, nullptr);
 
- Image result = img;
+ Image result = imgL;
+
  std::fill(result.pixel.begin(), result.pixel.end(), 0);
 
  // Get the result back to the host
  std::size_t origin[3] = { 0 };
- std::size_t region[3] = {img.width, img.height, 1 };
+ std::size_t region[3] = {imgL.width, imgL.height, 1 };
  clEnqueueReadImage(queue, outputImage, CL_TRUE,  origin, region, 0, 0,  result.pixel.data(), 0, nullptr, nullptr);
 
  lodepng::encode("img/niewiem.png", result.pixel , result.width, result.height);
@@ -95,6 +104,7 @@ int main()
  clReleaseMemObject(outputImage);
  clReleaseMemObject(filterWeightsBuffer);
  clReleaseMemObject(inputImage);
+ clReleaseMemObject(inputImage2);
  clReleaseCommandQueue(queue);
  clReleaseKernel(kernel);
  clReleaseProgram(program);
--- stereo_matching/kernels/image.cl
+++ stereo_matching/kernels/image.cl
@@ -3,14 +3,22 @@ __constant sampler_t sampler =
 | CLK_ADDRESS_CLAMP_TO_EDGE
 | CLK_FILTER_NEAREST;
 
-float FilterValue (__constant const float* filterWeights,
-	const int x, const int y)
+
+float FilterValue (__constant const float* filterWeights,	const int x, const int y)
 {
 	return filterWeights[(x+FILTER_SIZE) + (y+FILTER_SIZE)*(FILTER_SIZE*2 + 1)];
 }
-
+int ispixeleq(float4 a,float4 b)
+{
+ if (a[0] == b[0] && a[1] == b[1] && a[2] == b[2])
+ {
+  return 1;
+ }
+ return 0;
+}
 __kernel void Filter (
 	__read_only image2d_t input,
+ __read_only image2d_t input2,
 	__constant float* filterWeights,
 	__write_only image2d_t output)
 {
@@ -18,11 +26,12 @@ __kernel void Filter (
     const int2 pos = {get_global_id(0), get_global_id(1)};
 
     float4 sum = (float4)(0.0f);
-    for(int y = -FILTER_SIZE; y <= FILTER_SIZE; y++) {
-        for(int x = -FILTER_SIZE; x <= FILTER_SIZE; x++) {
-            sum += FilterValue(filterWeights, x, y)
-                * read_imagef(input, sampler, pos + (int2)(x,y));
-        }
+    for(int x = 0; x <= 50; x++) {
+     float4 a = read_imagef(input, sampler, pos);
+     float4 b = read_imagef(input2, sampler, pos + (int2)(x, 0));
+     if(ispixeleq(a,b))
+      sum += (float4)(0.0f, 0.5f, 0.0f, 1.0f)+ read_imagef(input2, sampler, pos+(int2)(x,0));
+     
     }
 
     write_imagef (output, (int2)(pos.x, pos.y), sum);
