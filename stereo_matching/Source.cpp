50239974237e52b392d30c50ded1fd91887accc1
Pawel M <obramanix@gmail.com>
2016-04-14 23:57:59 +0200

    cool but still not enough

--- stereo_matching/Source.cpp
+++ stereo_matching/Source.cpp
@@ -55,9 +55,9 @@ int main()
 
 
  Image imgL;
- lodepng::decode(imgL.pixel, imgL.width, imgL.height, "img/view1.png");
+ lodepng::decode(imgL.pixel, imgL.width, imgL.height, "sukub/imP.png");
  Image imgR;
- lodepng::decode(imgR.pixel, imgR.width, imgR.height, "img/view2.png");
+ lodepng::decode(imgR.pixel, imgR.width, imgR.height, "sukub/imL.png");
 
 
  // Create a program from source
@@ -75,14 +75,16 @@ int main()
  cl_mem inputImage2 = clCreateImage2D(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, &format, imgR.width, imgR.height, 0,  // This is a bug in the spec
   const_cast<unsigned char*> (imgR.pixel.data()), &error);
 
+ int mywidth = imgL.width;
  cl_mem outputImage = clCreateImage2D(context, CL_MEM_WRITE_ONLY, &format,  imgL.width, imgL.height, 0,  nullptr, &error);
 
  cl_mem filterWeightsBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,  sizeof(float) * 16, filter, &error);
-
+ cl_mem my_width = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(int) , &mywidth, &error);
  clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputImage);
  clSetKernelArg(kernel, 1, sizeof(cl_mem), &inputImage2);
  clSetKernelArg(kernel, 2, sizeof(cl_mem), &filterWeightsBuffer);
  clSetKernelArg(kernel, 3, sizeof(cl_mem), &outputImage);
+ clSetKernelArg(kernel, 4, sizeof(cl_mem), &my_width);
 
  cl_command_queue queue = clCreateCommandQueue(context, deviceIds[0],  0, &error);
  
@@ -99,7 +101,7 @@ int main()
  std::size_t region[3] = {imgL.width, imgL.height, 1 };
  clEnqueueReadImage(queue, outputImage, CL_TRUE,  origin, region, 0, 0,  result.pixel.data(), 0, nullptr, nullptr);
 
- lodepng::encode("img/niewiem.png", result.pixel , result.width, result.height);
+ lodepng::encode("sukub/niewiem.png", result.pixel , result.width, result.height);
 
  clReleaseMemObject(outputImage);
  clReleaseMemObject(filterWeightsBuffer);
--- stereo_matching/kernels/image.cl
+++ stereo_matching/kernels/image.cl
@@ -4,35 +4,40 @@ __constant sampler_t sampler =
 | CLK_FILTER_NEAREST;
 
 
-float FilterValue (__constant const float* filterWeights,	const int x, const int y)
-{
-	return filterWeights[(x+FILTER_SIZE) + (y+FILTER_SIZE)*(FILTER_SIZE*2 + 1)];
-}
 int ispixeleq(float4 a,float4 b)
 {
- if (a[0] == b[0] && a[1] == b[1] && a[2] == b[2])
+ if (a[0] <= b[0] + 0.01 && a[0] >= b[0] - 0.01 && a[1] <= b[1]+0.01 && a[1] >= b[1] - 0.01 && a[2] <= b[2] + 0.01 && a[2] >= b[2] - 0.01 )
  {
   return 1;
  }
+ else
  return 0;
 }
 __kernel void Filter (
 	__read_only image2d_t input,
  __read_only image2d_t input2,
 	__constant float* filterWeights,
-	__write_only image2d_t output)
+	__write_only image2d_t output,
+ __constant int* my_width)
 {
 
     const int2 pos = {get_global_id(0), get_global_id(1)};
 
     float4 sum = (float4)(0.0f);
-    for(int x = 0; x <= 50; x++) {
-     float4 a = read_imagef(input, sampler, pos);
-     float4 b = read_imagef(input2, sampler, pos + (int2)(x, 0));
-     if(ispixeleq(a,b))
-      sum += (float4)(0.0f, 0.5f, 0.0f, 1.0f)+ read_imagef(input2, sampler, pos+(int2)(x,0));
+    
+     float4 a1 = read_imagef(input, sampler, pos);
+     float4 a2 = read_imagef(input, sampler, pos + (int2)(1, 0));
+     float4 a3 = read_imagef(input, sampler, pos + (int2)(2, 0));
+     for (float x = 1; x <= *my_width; x ++) {
+      float4 b1 = read_imagef(input2, sampler, pos + (int2)(x, 0));
+      float4 b2 = read_imagef(input2, sampler, pos + (int2)(x + 1, 0));
+      float4 b3 = read_imagef(input2, sampler, pos + (int2)(x + 2, 0));
+      if (ispixeleq(a1, b1) || ispixeleq(a2, b2) || ispixeleq(a3, b3))//mogloby byc a1,b2 ; a1,b3 ->ale wtedy nie 'rozmyje'
+      {
+       sum = (float4)(0, x / (*my_width) * 30, 0, 1);
+       break;
+      }
      
     }
-
     write_imagef (output, (int2)(pos.x, pos.y), sum);
 }
